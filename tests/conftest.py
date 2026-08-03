6697ecd3c9a0eb6f0134ea605935c9b87fd05c3e
siswacpt025 <siswacpt025@student.wethinkcode.co.za>
2026-08-03 08:21:37 +0200

    test: add new fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,11 @@ from api_clients.booker_client import BookerClient
 
 @pytest.fixture
 def api_client():
-    """Fixture that initializes and provides the BookerClient instance."""
+    """Fixture that initialises and provides the BookerClient instance."""
     return BookerClient()
+
+@pytest.fixture
+def auth_token(api_client):
+    """Fixture that generates and provides an auth token."""
+    token = api_client.get_auth_token()
+    return token
