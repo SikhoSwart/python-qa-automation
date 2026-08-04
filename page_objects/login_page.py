40cf511443cb6795b10c785f1f4236c63a5488a9
siswacpt025 <siswacpt025@student.wethinkcode.co.za>
2026-08-04 12:31:23 +0200

    feat: error message locator

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -2,9 +2,13 @@
 class LoginPage:
     def __init__(self, page):
         self.page = page
+
+        self.url = "https://www.saucedemo.com/"
+
         self.username_input = page.locator("[data-test='username']")
         self.password_input = page.locator("[data-test='password']")
         self.login_button = page.locator("[data-test='login-button']")
+        self.error_message = page.locator("[data-test='error']")
 
     def login(self, username, password):
         self.username_input.fill(username)
