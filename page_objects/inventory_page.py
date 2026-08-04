1e7c2452589dd160da1071db32e71551c05fac96
siswacpt025 <siswacpt025@student.wethinkcode.co.za>
2026-08-04 13:16:21 +0200

    feat: create inventory page

---
+++ page_objects/inventory_page.py
@@ -0,0 +1,18 @@
+# page_objects/inventory_page.py
+
+class InventoryPage:
+    def __init__(self, page):
+        self.page = page
+
+        #Locators
+        self.title_label = page.locator(".title")
+        self.inventory_items = page.locator(".inventory_item")
+
+    def is_loaded(self) -> bool:
+        """Checks if the inventory page has loaded successfully."""
+        self.title_label.wait_for(state="visible")
+        return self.title_label.inner_text() == "Products"
+
+    def get_item_count(self) -> int:
+        """Returns the number of products displayed on the page."""
+        return self.inventory_items.count()
